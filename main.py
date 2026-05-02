66ef400fb3677c419643b6018f4d51215ff2293d
KhristinaKud <kud.christina@lll.kpi.ua>
2026-05-02 13:24:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f"Hi, {name}")
 
-def add (a,b):
-    return a+b
-def sub(a,b):
-    return a-b
-def mul(a,b):
-    return a*b
-def div(a,b):
-    return a/b
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    print(add(1,2))
-    print(sub(2,3))
-    print(mul(2,3))
-    print(div(2,3))
+
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def mul(a, b):
+    return a * b
+
+
+def div(a, b):
+    return a / b
+
+
+if __name__ == "__main__":
+    print_hi("PyCharm")
+    print(add(1, 2))
+    print(sub(2, 3))
+    print(mul(2, 3))
+    print(div(2, 3))
