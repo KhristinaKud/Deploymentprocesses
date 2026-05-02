1e867dc3173da1f9b8fd72da5566ba64191fa756
Khristina Kud <kud.christina@lll.kpi,ua>
2026-05-02 13:04:41 +0300

    add

---
+++ main.py
@@ -0,0 +1,18 @@
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def add (a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print(add(1,2))
+    print(sub(2,3))
+    print(mul(2,3))
+    print(div(2,3))
---
+++ test/test_main.py
@@ -0,0 +1,17 @@
+from main import add, sub, mul, div, print_hi
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_sub():
+    assert sub(10, 5) == 5
+
+def test_mul():
+    assert mul(2, 4) == 8
+
+def test_div():
+    assert div(10, 2) == 5
+def test_print_hi(capsys):
+    print_hi("Python")
+    captured = capsys.readouterr()
+    assert "Hi, Python" in captured.out
